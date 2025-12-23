2e3c63c538391c5241cf8d992203da5ffd0582ad
Ranjan <ranjan72525@gmail.com>
2025-12-23 22:53:36 +0530

    controlflow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,48 @@
+// const isUserloggedIn = true
+// const temp = 41
+// if( temp===41 ){
+//     console.log("less than 50");
+    
+// } else {
+// console.log("temperature is greater than 50");
+// }
+// console.log("executed");
+
+//<, >, <=, >=, ==, !=, ===, !==
+
+//+++++++blockscope+++++
+// const score = 200
+// if(score > 100){
+//     let power = "fly"
+//     console.log(`User power : ${power}`);
+    
+// }
+// console.log(`User power : ${power}`);
+
+// const balance = 1500
+
+// if(balance > 500) console.log("test");
+
+// if(balance < 500){
+//     console.log("less than");
+//   } else if(balance < 750){
+//     console.log("less thsn 750");
+    
+//   } else{
+//     console.log("less than 1200");
+    
+//   }
+
+const isUserloggedIn = true
+const debitCard = true
+const loggedInfromGoogle = false
+const loggedInfromEmail = true
+
+if(isUserloggedIn && debitCard && 2==3){
+    console.log("Allow to buy course");
+    
+}
+if(loggedInfromEmail || loggedInfromGoogle){
+    console.log("Logged in");
+    
+}
