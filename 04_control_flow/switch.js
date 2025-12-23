dca62f680cda500dbd9aea23ffa4c44911d2fb53
Ranjan <ranjan72525@gmail.com>
2025-12-23 23:09:58 +0530

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,57 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// +++++++++num+++++++
+
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("January");
+//         break;
+//     case 2:
+//         console.log("February");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     case 4:
+//         console.log("April");
+        
+//         break;
+
+//     default:
+//         console.log("default");
+        
+//         break;
+// }
+
+// +++++++++++string++++++
+
+const month = "apr"
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        
+        break;
+
+    default:
+        console.log("default");
+        
+        break;
+}
