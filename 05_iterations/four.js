8c574cfee95839cef39e6676d180aeb9363344ae
Ranjan <ranjan72525@gmail.com>
2025-12-24 22:32:17 +0530

    for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,18 @@
+const myobject = {
+    js: 'javascript',
+    cpp : 'c++',
+    rb : 'ruby',
+    swift  : 'swift apple'
+}
+
+for (const key in myobject) {
+//    console.log(`${key} shortcut is for ${myobject[key]}`);
+   
+    
+}
+const prgm  = ['javascript', 'rb', 'java', 'cpp']
+
+for (const key in prgm) {
+    console.log(`${key} vlaues ${prgm[key]}`);
+    
+}
