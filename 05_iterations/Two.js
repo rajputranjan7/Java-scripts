74b68dc75da1865c22885ccb8c586ca4de77014e
Ranjan <ranjan72525@gmail.com>
2025-12-24 21:42:57 +0530

    while do while

---
+++ 05_iterations/Two.js
@@ -0,0 +1,22 @@
+
+
+let i = 0
+// while ( i <=10 ) {
+//     console.log(`hello`);
+//     i+=2
+// }
+
+// let myArray = ['flash', 'ironman']
+// let arr = 0
+// while ( arr < myArray.length ) {
+//     console.log(`value is: ${myArray[arr]}`);
+//     arr = arr+1
+    
+// }
+
+let scr = 11
+do {
+    console.log(`Score is ${scr}`);
+    scr++
+    
+} while (scr <=10);
