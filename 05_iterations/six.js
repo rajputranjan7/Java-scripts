ea09ead780e21ecb68a590dbbfa811020732655d
Ranjan <ranjan72525@gmail.com>
2025-12-25 22:22:28 +0530

    filter

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -18,4 +18,25 @@ myNums.forEach( (num) => {
         newnums.push(num)
     }
 } )
-console.log(newnums);
+// console.log(newnums);
+
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, 
+        edition: 2004},
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1985, edition: 2008},
+    { title: 'Book Three', genre: 'History', publish: 2003, edition: 2009},
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1928, edition: 2010},
+    { title: 'Book Five', genre: 'Science', publish: 1999, edition: 2005},
+    { title: 'Book Six', genre: 'Fiction', publish: 2000,
+         edition: 2006},
+    { title: 'Book Seven', genre: 'History', publish: 2005, edition: 2010}
+];
+
+// const userBooks = books.filter( (bk) => bk.genre === 'History') //Syntax of filter
+
+let userBooks = books.filter( (bk) => {  
+  return  bk.publish >= 1995 && bk.genre === 'History'
+})
+console.log(userBooks);
+
