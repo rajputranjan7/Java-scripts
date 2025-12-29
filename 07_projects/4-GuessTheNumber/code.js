c0a65fd390bd39ee750ffc8865eb64a1520c6411
Ranjan <ranjan72525@gmail.com>
2025-12-29 23:53:08 +0530

    guessnumber..

---
+++ 07_projects/4-GuessTheNumber/code.js
@@ -0,0 +1,15 @@
+// random value input method
+const randomNumber = parseInt(Math.random() *100+1);
+
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remainnig = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+const p =document.createElement('p')
+
+
+
+
