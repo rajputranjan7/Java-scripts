9098a07dd7b4fa31bcd49a299f4558004cade4d3
Ranjan <ranjan72525@gmail.com>
2026-01-02 22:00:13 +0530

    .

--- 07_projects/4-GuessTheNumber/code.js
+++ 07_projects/4-GuessTheNumber/code.js
@@ -15,6 +15,47 @@ let numGuess = 1;
 
 let playGame = true;
 
+if(playGame){
+    submit.addEventListener('click', function(e){
+        e.preventDefault()
+        const guess = parseInt(userInput.value)
+        console.log(guess);
+        validateGuess(guess);
+        
+    })
+}
 
+function validateGuess(guess){
+if(isNaN(guess)){
+    alert('Please enter a valid number')
+} else if(gues<1){
+    alert('please enter a number more than 1')
+} else if(guess > 100){
+    alert('Please enter a number less than 100')
+}else{
+    prevGuess.push(guess)
+    if(numGuess === 11){
+        displayGuess(guess)
+        displayMessage(`Game Over. Random number was ${randomNumber}`)
+    }
+}
+}
 
+function checkGuess(guess){
 
+}
+
+function displayGuess(guess){
+
+}
+
+function displayMessage(message){
+
+}
+
+function endGame(){
+
+}
+function newGame(){
+    
+}
